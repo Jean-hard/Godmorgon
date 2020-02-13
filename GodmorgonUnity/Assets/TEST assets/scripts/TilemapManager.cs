6aceb6819486186139d26f291e8b2c111bb068c8
Paul Thibaul <Paulthb1@gmail.com>
2020-02-13 15:16:59 +0100

    Added First Proto
    tilemap, palette, player move, first card/button

---
+++ GodmorgonUnity/Assets/TEST assets/scripts/TilemapManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap ground;
+
+    private Vector3Int playerTilePosition;
+
+    private static TilemapManager _instance;
+
+    public static TilemapManager Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public Vector3 GetCurrentTileCenterPosition(Vector3 worldPosition)
+    {
+        Vector3 newPosition;
+        Vector3Int currentTilePos = ground.WorldToCell(worldPosition);
+
+        newPosition = ground.CellToWorld(currentTilePos) + new Vector3(0, 0.25f);
+        Debug.Log("position : " + newPosition);
+
+        return newPosition;
+    }
+
+    public Vector3Int GetTilePosition(Vector3 worldPosition)
+    {
+        return ground.WorldToCell(worldPosition);
+    }
+
+    public void PlayerTileOnColor(Vector3 worldPlayerPosition)
+    {
+        Debug.Log("YOLO ?");
+        //to give back it's original color to last tile where the player was
+        if (playerTilePosition != null)
+            ground.SetColor(playerTilePosition, Color.white);
+
+        playerTilePosition = ground.WorldToCell(worldPlayerPosition);
+
+        ground.SetTileFlags(playerTilePosition, TileFlags.None);
+        ground.SetColor(playerTilePosition, Color.black);
+    }
+}
---
+++ GodmorgonUnity/Assets/TEST assets/scripts/playerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField]
+    private float moveSpeed = 2.0f;
+
+    [SerializeField]
+    private Tilemap ground;
+
+    [SerializeField]
+    Vector3Int playerTilePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerTilePosition = ground.WorldToCell(transform.position);
+        //transform.position = ground.GetCellCenterWorld(playerTilePosition);
+        transform.position = ground.CellToWorld(playerTilePosition);
+        Debug.Log(" START player tilemap position : " + playerTilePosition);
+        Debug.Log(" START player position By Cell center : " + ground.GetCellCenterWorld(playerTilePosition));
+        Debug.Log(" START player position By Cell : " + ground.CellToWorld(playerTilePosition));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        playerTilePosition = ground.WorldToCell(transform.position);
+        //Debug.Log("player tilemap position : " + playerTilePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+        //transform.position = ground.GetCellCenterWorld(playerTilePosition);
+        transform.position = ground.CellToWorld(playerTilePosition);
+        //Debug.Log("player position By Cell : " + ground.GetCellCenterWorld(playerTilePosition));
+    }
+}
