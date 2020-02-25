f4cabecace984455ec8f35fe3c495ac6d2bf06d5
paul thibault <paulthb1@gmail.com>
2020-02-25 15:16:59 +0100

    fix .bat for CI

--- CI/ci_scripts/unity_build.bat
+++ CI/ci_scripts/unity_build.bat
@@ -1,1 +1,1 @@
-"d:\Program Files\Unity\2018.4.9f1\Editor\Unity.exe" -username "%UNITY_USER%" -password "%UNITY_PASSWORD%" -projectPath "%CD%" -batchmode -quit -nographics -logFile "runner.log" -executeMethod SeriousGaming.Tools.CI.Editor.FastBuilderCommand.LaunchFullBuild -buildLinuxUniversalPlayer D:\Miaou_Test\Ronron
+"d:\Program Files\Unity\2018.4.9f1\Editor\Unity.exe" -username "%UNITY_USER%" -password "%UNITY_PASSWORD%" -projectPath "%CD%" -batchmode -quit -nographics -logFile "runner.log" -executeMethod SeriousGaming.Tools.CI.Editor.FastBuilderCommand.LaunchFullBuild
