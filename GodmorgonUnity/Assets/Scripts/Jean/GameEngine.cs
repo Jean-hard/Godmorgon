628706ac2a25b1ae2287889c67589128d30d6c20
JeanDev <hardyjea@gmail.com>
2020-03-02 01:00:28 +0100

    player movements + game engine WIP

---
+++ GodmorgonUnity/Assets/Scripts/Jean/GameEngine.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine
+{
+    private GameState currentState;
+
+    private static GameEngine instance;
+
+    public static GameEngine Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEngine();
+            }
+
+            return instance;
+        }
+    }
+
+    public enum GameState
+    {
+        CHOOSEDECK,     //Le joueur doit choisir un deck
+        DRAFTING,       //Le joueur doit choisir une carte parmi 3
+        PLAYING,        //Au tour du joueur de jouer
+        MOVING,         //Le joueur se déplace
+        FIGHTING,       //Le joueur combat un PNJ
+        GAMEOVER,       //Le joueur a perdu
+        MENU,           //Le joueur est dans le menu principal
+        OPTIONS,        //Le joueur est dans le menu option
+        DEFAULT
+    };
+
+    //permet de get l'état courant, et set l'état courant en lançant ce qu'il doit se passer pour le nouvel état
+    public GameState CurrentState
+    {
+        get
+        {
+            if (currentState == GameState.DEFAULT)
+                Debug.LogError("Il y a un problème de GameState !");
+
+            return currentState;
+        }
+
+        set
+        {
+            if(currentState != value)
+                currentState = value;
+            
+            switch (currentState)
+            {
+                case GameState.CHOOSEDECK:
+                    // Lors du choix du deck
+                    break;
+                case GameState.DRAFTING:
+                    // Lors de la phase draft
+                    break;
+                case GameState.PLAYING:
+                    // Lorsque le joueur doit choisir quelle carte il joue
+                    break;
+                case GameState.MOVING:
+                    // Lorsque le joueur se déplace d'une case à l'autre
+                    break;
+                case GameState.FIGHTING:
+                    // Lors d'un combat entre joueur et PNJ
+                    break;
+                case GameState.GAMEOVER:
+                    // Lorsque le joueur est mort
+                    break;
+                case GameState.MENU:
+                    // Lorsque le joueur est sur le main menu
+                    break;
+                case GameState.OPTIONS:
+                    // Lorsque le joueur est dans le menu options
+                    break;
+                case GameState.DEFAULT:
+                    // On ne devrait jamais être dans cet état
+                    break;
+                default:
+                    // Si on n'est dans aucun de ces états
+                    break;
+            }
+        }
+    }   
+}
---
+++ GodmorgonUnity/Assets/Scripts/Jean/PlayerMove.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Grid moveGrid;
+    private float moveH, moveV;
+    private float gridX, gridY;
+    private Vector3 direction;
+
+    bool hasMoved;
+    /*
+    public void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int coordinate = moveGrid.WorldToCell(mouseWorldPos);
+            this.gameObject.transform.position = coordinate;
+            Debug.Log(coordinate);
+        }
+    }*/
+
+    void Start()
+    {
+        gridX = moveGrid.gameObject.GetComponent<Grid>().cellSize.x;
+        gridY = moveGrid.gameObject.GetComponent<Grid>().cellSize.y;
+    }
+
+    void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal");
+        moveV = Input.GetAxisRaw("Vertical");
+
+        if (moveH == 0)
+        {
+            hasMoved = false;
+        }
+        else if (moveH != 0 && !hasMoved)
+        {
+            hasMoved = true;
+
+            SetDirection();
+        }
+    }
+
+    public void SetDirection()
+    {
+        direction = Vector3.zero;
+
+        if (moveH < 0)
+        {
+            //si on veut bouger en haut à gauche
+            if (moveV > 0)
+            {
+                direction = new Vector3(-gridX / 2, gridY / 2);
+            }
+            //si on veut bouger en bas à gauche
+            else if (moveV < 0)
+            {
+                direction = new Vector3(-gridX / 2, -gridY / 2);
+            }
+            transform.position += direction;
+        }
+
+        else if (moveH > 0)
+        {
+            //si on veut bouger en haut à droite
+            if (moveV > 0)
+            {
+                direction = new Vector3(gridX / 2, gridY / 2);
+            }
+            //si on veut bouger en bas à droite
+            else if (moveV < 0)
+            {
+                direction = new Vector3(gridX / 2, -gridY / 2);
+            }
+            transform.position += direction;
+        }
+
+        TilemapManager.Instance.PlayerTileOnColor(transform.position);
+    }
+
+
+    public enum directionOrientation
+    {
+        UP_LEFT = 1,
+        UP_RIGHT,
+        DOWN_LEFT,
+        DOWN_RIGHT
+    }
+
+    public void Move(Vector3 direction)
+    {
+        transform.position += direction;
+    }
+
+    public void DirectionButton(int orientation)
+    {
+        direction = Vector3.zero;
+        switch (orientation)
+        {
+            //UP_LEFT
+            case 1:
+                direction = new Vector3(-0.5f, 0.25f);
+                break;
+            //UP_RIGHT
+            case 2:
+                direction = new Vector3(0.5f, 0.25f);
+                break;
+            //DOWN_LEFT
+            case 3:
+                direction = new Vector3(-0.5f, -0.25f);
+                break;
+            //DOWN_RIGHT
+            case 4:
+                direction = new Vector3(0.5f, -0.25f);
+                break;
+        }
+
+        transform.position += direction;
+        //TilemapManager.Instance.PlayerTileOnColor(transform.position);
+    }
+}
