9bce1b5fb44d3cbf5aa886b916544615d9223d11
JeanDev <hardyjea@gmail.com>
2020-03-19 19:58:52 +0100

    finished v1 enemy move, need link to timeline

---
+++ GodmorgonUnity/Assets/Scripts/Jean/Enemy/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodMorgon.Models
+{
+    [CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+    public class Enemy : ScriptableObject
+    {
+        public new string name;
+        public Vector3 nextPosition;
+
+    }
+}
