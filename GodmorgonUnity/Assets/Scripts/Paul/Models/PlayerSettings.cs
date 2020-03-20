eb9d245a0d6387afc849f072fbc65892aa5ea01c
Paul Thibaul <Paulthb1@gmail.com>
2020-03-20 16:56:04 +0100

    update GameScene for wireframe

---
+++ GodmorgonUnity/Assets/Scripts/Paul/Models/PlayerSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodMorgon.Models
+{
+    [CreateAssetMenu(fileName = "Player", menuName = "AddPlayerSettings")]
+    public class PlayerSettings : ScriptableObject
+    {
+        public int lifeMax;
+        public int life;
+        public int power;
+    }
+
+}
