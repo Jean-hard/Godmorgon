3fe2eb3960cf30a813c2e0f0bb67311b9bd25d11
JeanDev <hardyjea@gmail.com>
2020-03-25 18:31:52 +0100

    no error PlayerMgr

--- GodmorgonUnity/Assets/Scripts/Jean/Player/PlayerManager.cs
+++ GodmorgonUnity/Assets/Scripts/Jean/Player/PlayerManager.cs
@@ -95,16 +95,16 @@ public class PlayerManager : MonoBehaviour
             playerCanMove = true;
         }
 
-
+        /*
         if(playerCanMove)
         {
             float ratio = (float)spotIndex / (playerPathArray.Count - 1);   //ratio varie entre 0 et 1, 0 pour le spot le plus proche et 1 pour le spot final
             ratio = enemyMoveCurve.Evaluate(ratio);     //on le lie à notre curve pour le modifier dans l'inspector à notre guise
             float speed = enemySpeed * ratio;   //on le lie à la vitesse pour que la curve ait un impact sur la vitesse de l'enemy
             this.transform.position = Vector2.MoveTowards(this.transform.position,);
-        }
+        }*/
     }
-
+    /*
     public void SetPlayerPath(Vector3 tileClicked)
     {
         Vector3 playerPos = this.transform.position;
@@ -112,59 +112,57 @@ public class PlayerManager : MonoBehaviour
 
         playerPathArray = new List<Spot>();
 
-        foreach (GameObject enemy in enemiesArray)
+        spotIndex = 0;
+
+        int nbMoves = enemy.GetComponent<EnemyDisplay>().GetNbMoves();  //on récupère le nombre de room que peut parcourir un enemy (propre à chacun)
+
+        Vector3Int enemyPos = walkableTilemap.WorldToCell(enemy.transform.position);
+        if (roadPath != null && roadPath.Count > 0) //reset le roadpath
+            roadPath.Clear();
+
+        //création du path, prenant en compte la position des tiles, le point de départ, le point d'arrivée, et la longueur en tiles du path -> dépend de l'ennemi
+        //roadPath est une liste de spots = une liste de positions de tiles
+        roadPath = astar.CreatePath(spots, new Vector2Int(enemyPos.x, enemyPos.y), new Vector2Int(endPos.x, endPos.y), nbTilesToMove * nbMoves);
+
+        if (roadPath == null)
         {
-            spotIndex = 0;
-
-            int nbMoves = enemy.GetComponent<EnemyDisplay>().GetNbMoves();  //on récupère le nombre de room que peut parcourir un enemy (propre à chacun)
-
-            Vector3Int enemyPos = walkableTilemap.WorldToCell(enemy.transform.position);
-            if (roadPath != null && roadPath.Count > 0) //reset le roadpath
-                roadPath.Clear();
-
-            //création du path, prenant en compte la position des tiles, le point de départ, le point d'arrivée, et la longueur en tiles du path -> dépend de l'ennemi
-            //roadPath est une liste de spots = une liste de positions de tiles
-            roadPath = astar.CreatePath(spots, new Vector2Int(enemyPos.x, enemyPos.y), new Vector2Int(endPos.x, endPos.y), nbTilesToMove * nbMoves);
-
-            if (roadPath == null)
-            {
-                return;
-            }
-
-            foreach (Spot spot in roadPath)
-            {
-                bool hasPlayerOnPath = false;
-
-                //on met dans un tableau les enemies qui vont se rendre dans la room du player pour pouvoir les faire s'arreter une tile avant le player
-                if (playerCellPos.x == spot.X && playerCellPos.y == spot.Y)
-                {
-                    hasPlayerOnPath = true;
-                    enemiesCloseToPlayer.Add(enemiesArray[enemyIndex]);
-                }
-
-                if (!hasPlayerOnPath)
-                {
-                    playerPathArray[enemyIndex].Add(spot);     //on ajoute pour tel enemy les spots par lesquels il va devoir passer sauf celui où il y a le player
-                }
-
-                spotIndex++;
-            }
-
-            playerPathArray[enemyIndex].Reverse(); //on inverse la liste pour la parcourir de la tile la plus proche à la plus éloignée
-            playerPathArray[enemyIndex].RemoveAt(0);
-
-            /*
-                * Affiche les coordonnées de tiles des paths que les enemies doivent parcourir
-            foreach(Spot spot in enemiesPathArray[enemyIndex])
-            {
-                Debug.Log(spot.X + " / " + spot.Y);
-            }*/
-
-            enemyIndex++;
+            return;
         }
+
+        foreach (Spot spot in roadPath)
+        {
+            bool hasPlayerOnPath = false;
+
+            //on met dans un tableau les enemies qui vont se rendre dans la room du player pour pouvoir les faire s'arreter une tile avant le player
+            if (playerCellPos.x == spot.X && playerCellPos.y == spot.Y)
+            {
+                hasPlayerOnPath = true;
+                enemiesCloseToPlayer.Add(enemiesArray[enemyIndex]);
+            }
+
+            if (!hasPlayerOnPath)
+            {
+                playerPathArray[enemyIndex].Add(spot);     //on ajoute pour tel enemy les spots par lesquels il va devoir passer sauf celui où il y a le player
+            }
+
+            spotIndex++;
+        }
+
+        playerPathArray[enemyIndex].Reverse(); //on inverse la liste pour la parcourir de la tile la plus proche à la plus éloignée
+        playerPathArray[enemyIndex].RemoveAt(0);
+
+        /*
+            * Affiche les coordonnées de tiles des paths que les enemies doivent parcourir
+        foreach(Spot spot in enemiesPathArray[enemyIndex])
+        {
+            Debug.Log(spot.X + " / " + spot.Y);
+        }*/
+        /*
+        enemyIndex++;
+        
         
 
         playerCanMove = true;  //on autorise les enemies à bouger
         spotIndex = 0;
-    }
+    }*/
 }
