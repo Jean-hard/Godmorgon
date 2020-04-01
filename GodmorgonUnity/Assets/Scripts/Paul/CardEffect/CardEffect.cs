ddb68ddf28582a052b99043bdde79f33ba965ca9
Paul Thibaul <Paulthb1@gmail.com>
2020-04-01 19:37:58 +0200

    add cardEffect

---
+++ GodmorgonUnity/Assets/Scripts/Paul/CardEffect/CardEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GodMorgon.CardEffect
+{
+    /**
+     * Contain all the function for the possible card effect in the game
+     */
+    public class CardEffect
+    {
+        //true when the effect will be finished
+        protected bool isEffectDone = false;
+
+        public enum EFFECT_TYPE
+        {
+            DAMAGE
+
+        }
+
+        public void execute()
+        {
+
+        }
+
+        public bool EffectDone()
+        {
+            return true;
+        }
+    }
+}
