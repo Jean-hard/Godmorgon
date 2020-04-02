089c09e7ce4202b5e9ed50636126da2275c5eb43
Paul Thibaul <Paulthb1@gmail.com>
2020-04-02 12:02:11 +0200

    fix musicManager + assembly

---
+++ GodmorgonUnity/Assets/Scripts/Paul/Sound/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MusicManager : MonoBehaviour
+{
+    public AK.Wwise.Event musicEvent;
+
+    public AK.Wwise.State PlayerState;
+    public AK.Wwise.State RingmasterState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicEvent.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            PlayerState.SetValue();
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+            RingmasterState.SetValue();
+    }
+
+    public void 
+
+    
+}
