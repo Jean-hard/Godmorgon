a514d81c0a3b7211749a0e01770fc57aa15cec14
Julien <Julien.barreault77@gmail.com>
2020-04-23 14:40:16 +0200

    Add Asset effect particules FX

---
+++ GodmorgonUnity/Assets/Script/Curseur.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curseur : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public Texture2D defaultTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+        private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
