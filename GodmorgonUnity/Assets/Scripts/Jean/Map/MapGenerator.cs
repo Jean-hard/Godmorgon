f7f6b650e87de3d975c9cb54180ff6d961b83bfc
JeanDev <hardyjea@gmail.com>
2020-09-23 09:30:04 +0200

    Start map editor

---
+++ GodmorgonUnity/Assets/Scripts/Jean/Map/MapGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TreeEditor;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<Transform> nodePrefabList = new List<Transform>();
+    public NodeMap map;
+    private Node[] tempNodes;
+
+    public void GenerateMap()
+    {
+        string holderName = "Generated Map";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int x = 0; x < map.mapSize.x; x++)
+        {
+            for (int y = 0; y < map.mapSize.y; y++)
+            {
+                Vector3 nodePosition = new Vector3(x*3, 0, y*3);
+                Transform newNode = Instantiate(nodePrefabList[0], nodePosition, Quaternion.identity) as Transform;
+                newNode.parent = mapHolder;
+            }
+        }
+    }
+}
